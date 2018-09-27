7ae51fe303d7dc0d9fd8f334263a74c5a958c2ce
Saurabh Patel <srpatel@linkedin.com>
2018-09-27 11:03:08 -0700

    Small workaround or hack :)

--- app/src/main/java/com/saurabh/navupvsback/ActivityC.java
+++ app/src/main/java/com/saurabh/navupvsback/ActivityC.java
@@ -1,9 +1,13 @@
 package com.saurabh.navupvsback;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class ActivityC extends AppCompatActivity {
 
@@ -19,5 +23,28 @@ public class ActivityC extends AppCompatActivity {
     setSupportActionBar(myChildToolbar);
     ActionBar ab = getSupportActionBar();
     ab.setDisplayHomeAsUpEnabled(true);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      // Respond to the action bar's Up/Home button
+      case android.R.id.home:
+        Intent upIntent = NavUtils.getParentActivityIntent(this);
+        if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
+          // This activity is NOT part of this app's task, so create a new task
+          // when navigating up, with a synthesized back stack.
+          TaskStackBuilder.create(this)
+                          // Add all of this activity's parents to the back stack
+                          .addNextIntentWithParentStack(upIntent)
+                          // Navigate up to the closest parent
+                          .startActivities();
+        } else {
+          // simply go back
+          onBackPressed();
+        }
+        return true;
+    }
+    return super.onOptionsItemSelected(item);
   }
 }
